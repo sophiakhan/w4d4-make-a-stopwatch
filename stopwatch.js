a31e2dbf0741d986a9e96ed34c5b0d430fe1b8ba
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-25 11:44:58 -0500

    Adding updates before computer backup.

--- stopwatch.js
+++ stopwatch.js
@@ -1,17 +1,39 @@
 //creates an anonymous function for when the start button is clicked to run the startTimer function every 100 milliseconds.
 document.querySelector('#start').addEventListener('click', function() { setInterval(startTimer, 10)});
 
+// document.querySelector('#start').addEventListener('click', startTimer);
+// document.querySelector('#start').addEventListener('dblclick', resetWatch);
+// document.querySelector('#start').addEventListener(' ', timeout);
+
 var seconds = 0;
 var tenthSeconds = 0;
 var minutes = 0;
 
-function startTimer() {
-
+function startTimer(e) {
     tenthSeconds++;
     document.querySelector('#time').innerHTML = `${minutes} : ${seconds} : ${tenthSeconds}`;
 
-    if (`${tenthSeconds}`> '60') {
-        
-    }
+    if (tenthSeconds >= 99) {
+        tenthSeconds = 0;
+        seconds++;
+
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+        }
+    } 
+
 }
+
+function pauseTimer() {
+    document.querySelector('#start').replace 
+}
+
+
+// function changeToPause() {
+//     startButton = document.querySelector('#start').addEventListener('click');
+
+//     startButton.innerText === 'Pause';
+//     startButton.classList.add('btn-danger')
+// }
 
